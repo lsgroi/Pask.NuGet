4141767413a57c522ae89e00b6d3f60c6b7836cd
Luca Sgroi <l.sgroi@marketinvoice.com>
2017-03-07 13:43:19 +0000

    Add test infrastructure

---
+++ Pask.NuGet/tasks/Pack-Nuspec.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Import-Script Pask.NuGet.Tests.Infrastructure
+
+Describe "Pack-Nuspec" {
+    BeforeAll {
+        # Arrange
+        $TestSolutionFullPath = Join-Path $Here "Pack-Nuspec"
+        Install-PaskExtension -Name Pask.NuGet -SolutionFullPath $TestSolutionFullPath
+    }
+
+    Context "Pack without symbols package" {
+
+    }
+
+    Context "Pack with symbols package" {
+
+    }
+
+    Context "Pack including PDB files" {
+
+    }
+}
