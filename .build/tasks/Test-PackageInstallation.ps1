0767103b4aa865fdb34032051891299d66a18ecb
Luca Sgroi <lucasgroi@gmail.com>
2017-03-20 23:47:13 +0000

    Update Pask and test the dependency during package installation

--- .build/tasks/Test-PackageInstallation.ps1
+++ .build/tasks/Test-PackageInstallation.ps1
@@ -6,6 +6,8 @@ Task Test-PackageInstallation Clean, Pack-Nuspec, Push-Local, {
         $ApplicationDomainProject = $DTE.Solution.Projects | Where { $_.Name -eq "Application.Domain" }
         Assert ($ApplicationDomainProject.ProjectItems | Where { $_.Name -eq "version.txt" }) "Cannot find 'version.txt'"
         Assert ($ApplicationDomainProject.ProjectItems | Where { $_.Name -eq "readme.txt" }) "Cannot find 'readme.txt'"
+        $PaskVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.NuGet.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "Pask" }).version
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application.Domain'"
     }
 
     Test-PackageInstallation -Name Pask.NuGet -Assertion $Assertion -InstallationTargetInfo "Install into 'Application.Domain' project"
